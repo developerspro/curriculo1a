3287ac1d426e60109b751dc17620a2a4fd19d62f
Joao Vitor Diamon <gamerjv947@gmail.com>
2023-09-11 21:22:07 -0300

    alteraçao js

--- CurriculoJoaoDiamon.js
+++ CurriculoJoaoDiamon.js
@@ -1,13 +1,14 @@
-function typeWrite(elemento){
+function typeWrite (elemento) {
     const textoArray = elemento.innerHTML.split('');
-    elemento.innerHTML = ' ';
-    textoArray.forEach(function(letra, i){   
-      
-    setTimeout(function(){
-        elemento.innerHTML += letra;
-    }, 150 * i)
-
-  });
+    elemento.innerHTML = ''
+    textoArray.forEach((letra, i) => {
+        setTimeout(() =>  elemento.innerHTML += letra , 160 * i)
+    });
 }
-const titulo = document.querySelector('#nomepessoal');
-typeWrite(titulo);
+
+
+
+
+const titulo = document.querySelector('.nomepessoal');
+typeWrite (titulo);
+typeWrite (document.querySelector('.jefferson'))
